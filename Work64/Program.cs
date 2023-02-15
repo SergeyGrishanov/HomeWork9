e23ad9a5e58869da5c8179a736b5c271019142d8
Sergey <blackwhiterainbow@yandex.ru>
2023-02-15 20:18:46 +0300

    add Натуральные числа от N до 1

---
+++ Work64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(SeriesNum(n, m));
+
+int SeriesNum(int n, int m)
+{
+    if (n == m) return n;
+    else Console.Write($"{SeriesNum(n, m + 1)}, ");
+    return m;
+}
