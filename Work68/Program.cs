fd0280646abd74410ea534dcc957f37f5aba867d
Sergey <blackwhiterainbow@yandex.ru>
2023-02-15 20:20:17 +0300

    add Функция Аккермана с помощью рекурсии

---
+++ Work68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+Console.WriteLine("Введите положительное число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите положительное число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+
+    if (m > 0 && n == 0) return Ack(m - 1, 1);
+
+    else return Ack(m - 1, Ack(m, n - 1));
+}
+
+Console.WriteLine($"A({m}, {n}) = {Ack(m, n)}");
